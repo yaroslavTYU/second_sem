d0bed95c4d0195be772e05a2dfe307ec61615261
yaroslavTYU <tyutyunikov.yaroslav@gmail.com>
2026-03-13 15:32:31 +0300

    changed structure + sort

---
+++ src/apps/tasks/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Tasks
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import TaskSerlializer
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Tasks.objects.all().order_by('-created_at')
+    serializer_class = TaskSerlializer
+
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    
+    filterset_fileds = ['is_done', 'title', 'description']
+    ordering_fileds  = ['created_at', 'title', 'is_done']
+    ordering = ['-created_at']  
+
--- src/todo_project/urls.py
+++ src/todo_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls') )
+    path('api/', include('apps.tasks.urls') )
 ]
